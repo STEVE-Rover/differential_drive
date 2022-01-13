a0ccfa3429a714c84bcbfbb45ca8e82c57d25b36
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2022-01-13 17:30:46 -0500

    Updated documentation, adjusted hardware_interface for Zeus

--- README.md
+++ README.md
@@ -1,22 +1,45 @@
 # differential_drive
-
 This package calculates the odometry of a differential drive robot and publishes an odometry message along with the tf transform of the base link to the odom frame. It also takes twist commands and generates velocity commands for the right and left sides of the robot.
 
 ## diff_drive
+This is the node that receives the encoder data to calculate the odometry and also receives the velocity commands and converts them to left and right motor commands
 ### Subscribed topics
-* cmd_vel - (geometry_msgs/Twist): Twist commands
-* wheel_enc - (differential_drive/Encoders): Encoder values accounting for wrap around of both sides of the robot
+* cmd_vel (geometry_msgs/Twist): Twist commands
+* wheel_enc (differential_drive/Encoders): Encoder values accounting for wrap around of both sides of the robot
 
 ### Published topics
-* wheel_vel_target - (differential_drive/VelocityTargets): Velocity targets for both sides of the robot
-* odom - (nav_msgs/Odometry): Generated odometry message
+* wheel_vel_target (differential_drive/VelocityTargets): Velocity targets for both sides of the robot
+* odom (nav_msgs/Odometry): Generated odometry message
 
 ### Parameters
+* ~rate (float, default: 50): Rate (in Hz) at which the loop executes
 * ~base_width (float, default:0.2): Width between the left wheel to the right wheel
-* ~timeout_ticks (int, default:20): Number of loop iterations before a timeout occurs
 * ~ticks_per_meter (int, default:50): Number of encoder ticks per meter
 * ~encoder_min (int, default:-32768): Minimum encoder value
 * ~encoder_max (int, default:32768): Maximum encoder value
 * ~base_frame_id (string, default:"base_link"): Name of base frame
 * ~odom_frame_id (string, default:"odom"): Name of odom frame
 * ~publish_tf (bool, default:true): Broadcast tf transform for base_link to odom
+
+## hardware_interface
+The purpose of this node is to act as the middle man between the differential drive controller and the encoders and the motor controllers. This is where encoder wrap around should be handled and where the motor commands should be modified to be compatible with your hardware. <br>
+In other words, this node is responsible for making this package compatible with your specific hardware. It should be modified, or rewritten to fit your needs.
+
+### Subscribed topics
+* wheel_vel_target (differential_drive/VelocityTargets): Velocity targets for both sides of the robot
+* TODO: individual encoder topics. 
+### Published topics
+* wheel_enc (differential_drive/Encoders): Encoder values accounting for wrap around of both sides of the robot
+* motor_cmd_left (std_msgs/Int32): Motor value [0 - 100] for the left side of the robot
+* motor_cmd_right (std_msgs/Int32): Motor value [0 - 100] for the right side of the robot
+* /zeus/left_front_wheel_velocity_controller/command (std_msgs/Float64): Motor command when parameter `simulation` is set to true. There are also similarly named topics for the other five wheels.
+
+### Parameters
+* ~rate (float, default: 50): Rate (in Hz) at which the loop executes
+* ~max_speed (float, default: 0.5): Maximum forward speed of the robot
+* ~encoder_min (int, default: -32768): Minimum encoder value
+* ~encoder_max (int, default: 32768): Maximum encoder value
+* ~simulation (bool, default: false): If the robot is run in a simulation
+
+## Improvements
+* Make `hardware_interface` a part of `diff_drive` instead of two separate nodes to improve performance.
--- src/hardware_interface.py
+++ src/hardware_interface.py
@@ -1,41 +1,51 @@
 #!/usr/bin/env python
 
-# Description: This node takes as input the velocity target of the wheels and converts it to motor values. 
-#              It also publishes the encoder values for both sides
-# Author: Jeremie Bourque - jeremie.bourque@usherbrooke.ca
-
-import time
 import rospy
-import threading
 from copy import deepcopy
 from differential_drive.msg import VelocityTargets, Encoders
-from std_msgs.msg import Int32, Float32
+from std_msgs.msg import Float32, Float64, Int32
+from sensor_msgs.msg import JointState
 
 class MotorVelocityController:
     def __init__(self):
-        rospy.Subscriber("wheel_vel_target", VelocityTargets, self.velocityTargetsCB, queue_size=1)
-        rospy.Subscriber("/ros_talon1/current_position", Float32, self.lfwheelCB)
-        rospy.Subscriber("/ros_talon2/current_position", Float32, self.rfwheelCB)
-        rospy.Subscriber("/ros_talon5/current_position", Float32, self.lbwheelCB)
-        rospy.Subscriber("/ros_talon6/current_position", Float32, self.rbwheelCB)
-        self.pub_wheel_enc = rospy.Publisher("wheel_enc", Encoders, queue_size=1)
-        self.pub_motor_cmd_left = rospy.Publisher("motor_cmd_left", Int32, queue_size=1)
-        self.pub_motor_cmd_right = rospy.Publisher("motor_cmd_right", Int32, queue_size=1)
         self.upper_limit = 100
         self.lower_limit = -100
-        self.maximum_speed = 0.5  # m/s
-        self.timeout_ticks = rospy.get_param("~timeout_ticks", 10)
-        self.ticks = 0
+        self.rate = rospy.get_param("~rate", 50)
+        self.maximum_speed = rospy.get_param("~max_speed", 0.5)  # m/s
         self.encoder_min = rospy.get_param("~encoder_min", -32768)
         self.encoder_max = rospy.get_param("~encoder_max", 32768)
-        self.encoder_low_wrap = rospy.get_param("~wheel_low_wrap", (self.encoder_max - self.encoder_min) * 0.3 + self.encoder_min)
-        self.encoder_high_wrap = rospy.get_param("~wheel_high_wrap", (self.encoder_max - self.encoder_min) * 0.7 + self.encoder_min)
+        self.encoder_low_wrap = (self.encoder_max - self.encoder_min) * 0.3 + self.encoder_min
+        self.encoder_high_wrap = (self.encoder_max - self.encoder_min) * 0.7 + self.encoder_min
+        self.simulation = rospy.get_param("~simulation", False)
 
         self.raw_enc = [0.0] * 4  # Wheel encoders with limits
         self.prev_raw_enc = [0.0] * 4  # Previous wheel encoders with limits
         self.enc_multipliers = [0.0] * 4  # Number of times the encoders have wrapped around
         self.wrapped_enc = [0.0] * 4  # Wheel encoders with wrap around
 
+        rospy.Subscriber("wheel_vel_target", VelocityTargets, self.velocityTargetsCB, queue_size=1)
+        if self.simulation:
+            rospy.Subscriber("/zeus/joint_states", JointState, self.jointStateCB)
+        else:
+            rospy.Subscriber("/ros_talon1/current_position", Float32, self.lfwheelCB)
+            rospy.Subscriber("/ros_talon2/current_position", Float32, self.rfwheelCB)
+            rospy.Subscriber("/ros_talon5/current_position", Float32, self.lbwheelCB)
+            rospy.Subscriber("/ros_talon6/current_position", Float32, self.rbwheelCB)
+        
+        self.pub_wheel_enc = rospy.Publisher("wheel_enc", Encoders, queue_size=1)
+        if self.simulation:
+            self.pub_lf_cmd = rospy.Publisher("/zeus/left_front_wheel_velocity_controller/command", Float64, queue_size=1)
+            self.pub_lm_cmd = rospy.Publisher("/zeus/left_middle_wheel_velocity_controller/command", Float64, queue_size=1)
+            self.pub_lr_cmd = rospy.Publisher("/zeus/left_rear_wheel_velocity_controller/command", Float64, queue_size=1)
+            self.pub_rf_cmd = rospy.Publisher("/zeus/right_front_wheel_velocity_controller/command", Float64, queue_size=1)
+            self.pub_rm_cmd = rospy.Publisher("/zeus/right_middle_wheel_velocity_controller/command", Float64, queue_size=1)
+            self.pub_rr_cmd = rospy.Publisher("/zeus/right_rear_wheel_velocity_controller/command", Float64, queue_size=1)
+        else:
+            self.pub_motor_cmd_left = rospy.Publisher("motor_cmd_left", Int32, queue_size=1)
+            self.pub_motor_cmd_right = rospy.Publisher("motor_cmd_right", Int32, queue_size=1)
+
+    def jointStateCB(self, msg):
+        self.raw_enc = msg.position[0:6]
     def lfwheelCB(self, msg):
         self.raw_enc[0] = msg.data
         self.handleWrapAround(0)
@@ -50,13 +60,9 @@ class MotorVelocityController:
         self.handleWrapAround(3)
 
     def velocityTargetsCB(self, msg):
-        left_cmd = Int32()
-        right_cmd = Int32()
-        left_cmd.data = self.limitValue(-msg.left_wheel_vel_target / self.maximum_speed * self.upper_limit)
-        right_cmd.data = self.limitValue(msg.right_wheel_vel_target / self.maximum_speed * self.upper_limit)
-        self.pub_motor_cmd_left.publish(left_cmd)
-        self.pub_motor_cmd_right.publish(right_cmd)
-        self.ticks = 0
+        left_cmd = self.limitValue(-msg.left_wheel_vel_target / self.maximum_speed * self.upper_limit)
+        right_cmd = self.limitValue(msg.right_wheel_vel_target / self.maximum_speed * self.upper_limit)
+        self.publishMotorCmds(left_cmd, right_cmd)
     
     def limitValue(self, value):
         if value > self.upper_limit:
@@ -74,18 +80,33 @@ class MotorVelocityController:
         self.wrapped_enc[i] = self.raw_enc[i] + self.enc_multipliers[i] * (self.encoder_max - self.encoder_min)
         self.prev_raw_enc[i] = self.raw_enc[i]
 
+    def publishMotorCmds(self, left, right):
+        if self.simulation:
+            left_cmd = Float64()
+            right_cmd = Float64()
+            left_cmd.data = left
+            right_cmd.data = right
+            self.pub_lf_cmd.publish(left_cmd)
+            self.pub_lm_cmd.publish(left_cmd)
+            self.pub_lr_cmd.publish(left_cmd)
+            self.pub_rf_cmd.publish(right_cmd)
+            self.pub_rm_cmd.publish(right_cmd)
+            self.pub_rr_cmd.publish(right_cmd)
+        else:
+            left_cmd = Int32()
+            right_cmd = Int32()
+            left_cmd.data = left
+            right_cmd.data = right
+            self.pub_motor_cmd_left.publish(left_cmd)
+            self.pub_motor_cmd_right.publish(right_cmd)
+
     def loop(self):
         rate = rospy.Rate(50)
         while not rospy.is_shutdown():
-            # if self.ticks > self.timeout_ticks:
-            #     # Stop motors
-            #     self.pub_motor_cmd_left.publish(Int32())
-            #     self.pub_motor_cmd_right.publish(Int32())
             wheel_enc = Encoders()
             wheel_enc.left_encoder = -(int((self.wrapped_enc[0] + self.wrapped_enc[1])/2))
             wheel_enc.right_encoder = (int((self.wrapped_enc[2] + self.wrapped_enc[3])/2))
             self.pub_wheel_enc.publish(wheel_enc)
-            self.ticks += 1
         rate.sleep()
     
 
@@ -97,8 +118,7 @@ if __name__ == '__main__':
         motor_velocity_controller.loop()
     except KeyboardInterrupt:
         # Stop motors
-        MotorVelocityController.pub_motor_cmd_left.publish(Int32())
-        MotorVelocityController.pub_motor_cmd_right.publish(Int32())
+        MotorVelocityController.publishMotorCmds(0, 0)
         raise
     
 
